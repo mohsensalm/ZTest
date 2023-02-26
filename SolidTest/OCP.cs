f6d105c7e4faead25e00fce92e50d5f436f52c21
Pc <Pc@DESKTOP-PRAMDG2>
2023-02-26 10:57:59 +0330

    Add project files.

---
+++ SolidTest/OCP.cs
@@ -0,0 +1,56 @@
+﻿namespace SolidTest
+{
+    public interface IUploadOrderFile
+    {
+        object ProcessOrderFile();
+    }
+
+    public class UploadXMLOrderFile : IUploadOrderFile
+    {
+        public object ProcessOrderFile()
+        {
+            object orderObj = null;
+
+            //Parse XML File to DTO Object
+
+            return orderObj;
+        }
+    }
+
+    public class UploadProcess
+    {
+        public bool UploadFile()
+        {
+            IUploadOrderFile orderFile = null;
+
+            //if XML File
+            {
+                orderFile = new UploadXMLOrderFile();
+            }
+
+            //if JSON File
+            {
+                orderFile = new UploadJSONOrderFile();
+            }
+
+            Object orderObj = orderFile.ProcessOrderFile();
+
+            //Validate Records
+
+            //Save Records
+
+            return true;
+        }
+    }
+    public class UploadJSONOrderFile : IUploadOrderFile
+    {
+        public object ProcessOrderFile()
+        {
+            object orderObj = null;
+
+            //Parse JSON File to DTO Object
+
+            return orderObj;
+        }
+    }
+}
---
+++ SolidTest/SRP.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace SolidTest
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        IEmailService _emailService = null;
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public bool SendEmail(EmailData emailData)
+        {
+            return _emailService.SendEmail(emailData);
+        }
+    }
+    public class EmailService : IEmailService
+    {
+        EmailSettings _emailSettings = null;
+        public EmailService(IOptions<EmailSettings> options)
+        {
+            _emailSettings = options.Value;
+        }
+
+        public bool SendEmail(EmailData emailData)
+        {
+            try
+            {
+                MimeMessage emailMessage = new MimeMessage();
+
+                MailboxAddress emailFrom = new MailboxAddress(_emailSettings.Name, _emailSettings.EmailId);
+                emailMessage.From.Add(emailFrom);
+
+                MailboxAddress emailTo = new MailboxAddress(emailData.EmailToName, emailData.EmailToId);
+                emailMessage.To.Add(emailTo);
+
+                emailMessage.Subject = emailData.EmailSubject;
+
+                BodyBuilder emailBodyBuilder = new BodyBuilder();
+                emailBodyBuilder.TextBody = emailData.EmailBody;
+                emailMessage.Body = emailBodyBuilder.ToMessageBody();
+
+                SmtpClient emailClient = new SmtpClient();
+                emailClient.Connect(_emailSettings.Host, _emailSettings.Port, _emailSettings.UseSSL);
+                emailClient.Authenticate(_emailSettings.EmailId, _emailSettings.Password);
+                emailClient.Send(emailMessage);
+                emailClient.Disconnect(true);
+                emailClient.Dispose();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log Exception Details
+                return false;
+            }
+        }
+    }
+
+}

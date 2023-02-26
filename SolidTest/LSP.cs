1b29fef6ba0a8e1c0aa70b4264e035d4521950c1
Pc <Pc@DESKTOP-PRAMDG2>
2023-02-26 12:15:33 +0330

    3

---
+++ SolidTest/LSP.cs
@@ -0,0 +1,47 @@
+﻿namespace SolidTest
+{
+    public abstract class MotorInsurance
+    {
+        public abstract bool IssuePolicy();
+
+        public virtual bool GetPassengerCover()
+        {
+            return false;
+        }
+    }
+
+    public class TwoWheelerInsurance : MotorInsurance
+    {
+        public override bool IssuePolicy()
+        {
+            //Logic to Issue & Generate Policy
+
+            return true;
+        }
+    }
+
+    public class PrivateCarInsurance : MotorInsurance
+    {
+        public override bool IssuePolicy()
+        {
+            //Logic to Issue & Generate Policy
+
+            return true;
+        }
+    }
+
+    public class CommercialVehicleInsurance : MotorInsurance
+    {
+        public override bool IssuePolicy()
+        {
+            //Logic to Issue & Generate Policy
+
+            return true;
+        }
+
+        public override bool GetPassengerCover()
+        {
+            return true;
+        }
+    }
+}
--- SolidTest/Program.cs
+++ SolidTest/Program.cs
@@ -1,6 +1,18 @@
+using SolidTest;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+MotorInsurance motorInsurance = new PrivateCarInsurance();
+Console.WriteLine("PrivateCarInsurance => PassengerCover => " + motorInsurance.GetPassengerCover());
+
+motorInsurance = new TwoWheelerInsurance();
+Console.WriteLine("TwoWheelerInsurance => PassengerCover => " + motorInsurance.GetPassengerCover());
+
+motorInsurance = new CommercialVehicleInsurance();
+Console.WriteLine("CommercialVehicleInsurance => PassengerCover => " + motorInsurance.GetPassengerCover());
+
+Console.ReadKey();
